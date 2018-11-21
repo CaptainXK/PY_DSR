17cc9620d3da00b7e15ad42d46a7aa9f44aaee0d
xuxu1992 <permission@hnu.edu.cn>
2018-11-22 00:27:19 +0800

    add api to check out whether can communicate with other node

--- Node.py
+++ Node.py
@@ -1,3 +1,5 @@
+import math
+
 class Node:
     m_x=0
     m_y=0
@@ -10,3 +12,19 @@ class Node:
     
     def get_pos(self):
         return [self.m_x, self.m_y]
+    
+    def get_range(self):
+        return self.m_range
+
+    def can_touch(self, _node):
+        _pos = _node.get_pos()
+        _range = _node.get_range()
+
+        #calculate distance
+        _dis = int(math.sqrt(pow(abs(_pos[0] - self.m_x), 2) + pow(abs(_pos[1] - self.m_y), 2) ))
+
+        #can touch?
+        if _dis <= self.m_range and _dis <= _range:
+            return True
+        else:
+            return False

70c92f68f1f421cfd97ceba3d7c1f701bb5d23c2
xk <permission@hnu.edu.cn>
2018-11-23 17:55:57 +0800

    use un-block RLock

--- Pipe.py
+++ Pipe.py
@@ -7,65 +7,55 @@ class Pipe:
     m_msg_nb=0
 
     def __init__(self): 
-        self.rw_lock = threading.Lock()
+        self.rw_lock = threading.RLock()
         self.m_buf = Queue()
         self.m_msg_nb=0
 
-    # try lock 
-    def do_lock(self):
-        self.rw_lock.acquire()
+    # # try lock 
+    # def do_lock(self):
+    #     self.rw_lock.acquire()
     
-    # try release
-    def do_release(self):
-        self.rw_lock.release()
+    # # try release
+    # def do_release(self):
+    #     self.rw_lock.release()
 
     # put msg onto buf 
     def send(self, _msg):
-        self.do_lock()
-
-        self.m_buf.put(_msg)
-
-        self.m_msg_nb += 1
-        
-        self.do_release()
-
-    # flush
-    def send_all(self, _msgs):
-        self.do_lock()
-
-        for _msg in _msgs:
+        with self.rw_lock:
             self.m_buf.put(_msg)
             self.m_msg_nb += 1
         
-        self.do_release()
+
+    # flush
+    def send_all(self, _msgs):
+        with self.rw_lock:
+            for _msg in _msgs:
+                self.m_buf.put(_msg)
+                self.m_msg_nb += 1
+        
 
     # get msg from buf
     def recv(self):
         _msg = None
         
-        self.do_lock()
-
-        if not self.m_buf.empty():
-            _msg = self.m_buf.get()
+        with self.rw_lock:
+            if not self.m_buf.empty():
+                _msg = self.m_buf.get()
         
-        self.m_msg_nb -= 1
-        
-        self.do_release()
+            self.m_msg_nb -= 1
 
         return _msg
 
     def recv_all(self):
         _msgs = []
         
-        self.do_lock()
-
-        while not self.m_buf.empty():
-            _msg = self.m_buf.get()
-            _msgs.append(_msg)
+        with self.rw_lock:
+            while not self.m_buf.empty():
+                _msg = self.m_buf.get()
+                _msgs.append(_msg)
         
-        self.m_msg_nb = 0
+            self.m_msg_nb = 0
         
-        self.do_release()
 
         return _msgs
 

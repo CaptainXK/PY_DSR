79cb3a5a35601e6cf3ccfeedca380ae4b0c8ac63
xk <permission@hnu.edu.cn>
2018-11-23 15:30:40 +0800

    lock and release move into member func

--- Pipe.py
+++ Pipe.py
@@ -1,30 +1,73 @@
 import threading
-from queue import *
+from queue import Queue
 
 class Pipe:
     rw_lock=None
     m_buf=None
+    m_msg_nb=0
 
-    def __init__(self):
+    def __init__(self): 
         self.rw_lock = threading.Lock()
         self.m_buf = Queue()
-    
-    def __send__(self, _msg):
+        self.m_msg_nb=0
+
+    # try lock 
+    def do_lock(self):
         self.rw_lock.acquire()
+    
+    # try release
+    def do_release(self):
+        self.rw_lock.release()
+
+    # put msg onto buf 
+    def send(self, _msg):
+        self.do_lock()
 
         self.m_buf.put(_msg)
+
+        self.m_msg_nb += 1
         
-        self.rw_lock.release()
+        self.do_release()
 
-    def __recv__(self):
-        self.rw_lock.acquire()
+    # flush
+    def send_all(self, _msgs):
+        self.do_lock()
 
+        for _msg in _msgs:
+            self.m_buf.put(_msg)
+            self.m_msg_nb += 1
+        
+        self.do_release()
+
+    # get msg from buf
+    def recv(self):
         _msg = None
+        
+        self.do_lock()
 
-        if len(self.m_buf) > 0:
+        if not self.m_buf.empty():
             _msg = self.m_buf.get()
         
-        self.rw_lock.release()
+        self.m_msg_nb -= 1
+        
+        self.do_release()
 
         return _msg
+
+    def recv_all(self):
+        _msgs = []
+        
+        self.do_lock()
+
+        while not self.m_buf.empty():
+            _msg = self.m_buf.get()
+            _msgs.append(_msg)
+        
+        self.m_msg_nb = 0
+        
+        self.do_release()
+
+        return _msgs
+
+
 
